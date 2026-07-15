0c1acb64e64082dd38aee788ad7f49225bc06d98
cabana-diane <dcabanao@est.unap.edu.pe>
2026-07-14 23:50:58 -0500

    another change

--- android/app/src/main/kotlin/com/example/flutter_application_1/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/flutter_application_1/MainActivity.kt
@@ -1,5 +1,11 @@
-package com.example.flutter_application_1
+/*package com.example.flutter_application_1
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity : FlutterActivity()
+class MainActivity : FlutterActivity()*/
+
+package com.example.flutter_application_1
+
+import io.flutter.embedding.android.FlutterFragmentActivity
+
+class MainActivity: FlutterFragmentActivity()
--- lib/core/services/biometric_service.dart
+++ lib/core/services/biometric_service.dart
@@ -9,7 +9,7 @@ class BiometricService {
 
   /// Indica si el dispositivo soporta biometría y tiene al menos
   /// una huella/Face ID registrada.
-  Future<bool> isDeviceSupported() async {
+  /*Future<bool> isDeviceSupported() async {
     try {
       final canCheck = await _auth.canCheckBiometrics;
       final isSupported = await _auth.isDeviceSupported();
@@ -17,7 +17,40 @@ class BiometricService {
     } catch (e) {
       return false;
     }
+  }*/
+  Future<bool> isDeviceSupported() async {
+    try {
+      final isSupported = await _auth.isDeviceSupported();
+
+      final biometrics =
+          await _auth.getAvailableBiometrics();
+
+      print("Soporta biometría: $isSupported");
+      print("Biometrías disponibles: $biometrics");
+
+      return isSupported && biometrics.isNotEmpty;
+
+    } catch (e) {
+      print("Error comprobando biometría: $e");
+      return false;
+    }
   }
+
+  Future<void> testBiometric() async {
+  try {
+    final supported = await _auth.isDeviceSupported();
+
+    final biometrics =
+        await _auth.getAvailableBiometrics();
+
+    print("Dispositivo soporta: $supported");
+    print("Biometrías: $biometrics");
+
+  } catch(e) {
+    print("Error: $e");
+  }
+}
+
 
   /// Indica si el USUARIO activó el login biométrico dentro de tu app.
   Future<bool> isBiometricEnabled() async {
@@ -37,10 +70,28 @@ class BiometricService {
     await prefs.remove(_kBiometricEmailKey);
   }
 
-  Future<bool> authenticate({required String reason}) async {
+  /*Future<bool> authenticate({required String reason}) async {
     try {
       return await _auth.authenticate(localizedReason: reason);
     } catch (e) {
+      return false;
+    }
+  }*/
+
+  Future<bool> authenticate({required String reason}) async {
+    try {
+      final authenticated = await _auth.authenticate(
+        localizedReason: reason,
+        biometricOnly: true,
+        sensitiveTransaction: true,
+      );
+
+      print("Resultado autenticación: $authenticated");
+
+      return authenticated;
+
+    } catch (e) {
+      print("Error autenticación biométrica: $e");
       return false;
     }
   }
